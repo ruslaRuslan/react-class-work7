d8300a209eaeda0543032691ef64560f27614b5c
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-08 13:18:38 +0400

    WebRouter - i App-de caqirdix

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,7 +4,7 @@ const WebRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<div>salam home page</div>} />
-      <Route path="/about" element={<div>salam about page</div>} />
+      <Route path="/about" element={<div>salam About page</div>} />
     </Routes>
   );
 };
